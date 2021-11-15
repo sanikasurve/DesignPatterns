d86dd330c3c4e0a62c39458cff1863ceb4bc543e
Sanika Surve <89775023+sanikasurve@users.noreply.github.com>
2021-11-15 18:52:50 +0530

    feat: template pattern code added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,12 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        CPU cpu = new CPU();
-        Memory memory = new Memory();
-        HardDrive hd = new HardDrive();
+        Game game = new EndlessRunnerGame();
+        game.play();
 
 
-        ComputerFacade computerFacade = new ComputerFacade(cpu, memory, hd);
-        computerFacade.start();
     }
 }
