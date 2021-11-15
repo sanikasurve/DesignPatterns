a6831e19498d6b0cc00d25bf94d2481661ba1f73
Sanika Surve <89775023+sanikasurve@users.noreply.github.com>
2021-11-15 23:49:35 +0530

    feat: MVC design pattern code added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,39 @@
 package com.company;
 
+import com.company.controller.EmployeeController;
+import com.company.model.Employee;
+import com.company.view.EmployeeDashboardView;
+
 public class Main {
 
     public static void main(String[] args) {
-	 Bank bank = new ProxyBank();
 
-        try {
-            bank.withdrawMoney("Sanika");
-            bank.withdrawMoney("me@me");
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+        Employee employee = retrieveEmployeeFromServer();
+
+        //Create our view to which we'll write our employee information into
+        EmployeeDashboardView view = new EmployeeDashboardView();
+
+        //Create our controller
+        EmployeeController controller = new EmployeeController(employee, view);
+
+
+
+//        controller.setEmployee(employee.getFirstName(), employee.getLastName());
+
+        controller.updateDashboardView();
+
+
+
+    }
+
+    public static Employee retrieveEmployeeFromServer() {
+        Employee employee = new Employee();
+        employee.setSsNumber("8643257");
+        employee.setFirstName("Ram");
+        employee.setLastName("Sharma");
+        employee.setSalary(500000);
+
+        return employee;
+
     }
 }
---
+++ src/com/company/controller/EmployeeController.java
@@ -0,0 +1,36 @@
+package com.company.controller;
+
+import com.company.model.Employee;
+import com.company.view.EmployeeDashboardView;
+
+public class EmployeeController {
+    private Employee employeeModel;
+    private EmployeeDashboardView view;
+
+    public EmployeeController(Employee employeeModel, EmployeeDashboardView view) {
+        this.employeeModel = employeeModel;
+        this.view = view;
+    }
+
+    public void setEmployee(String name, String lastName) {
+        employeeModel.setFirstName(name);
+        employeeModel.setLastName(lastName);
+    }
+
+    public String getEmployeeName() {
+        return employeeModel.getFirstName() + " " + employeeModel.getLastName();
+    }
+
+    public void setSSN(String ssn) {
+        employeeModel.setSsNumber(ssn);
+    }
+
+    public String getSSN() {
+        return employeeModel.getSsNumber();
+    }
+
+    //Update our view
+    public void updateDashboardView() {
+        view.printEmployeeInformation(employeeModel);
+    }
+}
