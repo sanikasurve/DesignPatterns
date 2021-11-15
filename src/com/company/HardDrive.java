0f84addcd6aa598b901f4c2edab43898e67ffec0
Sanika Surve <89775023+sanikasurve@users.noreply.github.com>
2021-11-15 18:45:20 +0530

    feat: facade pattern code added

---
+++ src/com/company/HardDrive.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class HardDrive {
+    public byte[] read(long Iba, int size) {
+        return new byte[]{'f', 'z'};
+    }
+}
---
+++ src/com/company/Memory.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Memory {
+    public void load(long position, byte[] data) {
+        System.out.println("Added item to memory.." + position);
+    }
+}
