e7229b27e2dfb01265ec7437bdb334a1ed8bb405
Sanika Surve <89775023+sanikasurve@users.noreply.github.com>
2021-11-21 18:59:58 +0530

    feat: chain of responsibility pattern code added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,33 +1,35 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public InterpreterContext interpreterContext;
 
     public static void main(String[] args) {
-        String first = "28 in Binary";
-        String second = "28 in Hexadecimal";
 
-        Main interpreter = new Main(new InterpreterContext());
-        System.out.println(first+ " = " + interpreter.interpret(first));
-        System.out.println(second+ " = " + interpreter.interpret(second));
+        CEOPurchasePower ceoPurchasePower = new CEOPurchasePower();
+        DirectorPurchasePower directorPurchasePower = new DirectorPurchasePower();
+        ManagerPurchasePower managerPurchasePower = new ManagerPurchasePower();
+
+        ceoPurchasePower.setSuccessor(directorPurchasePower);
+        directorPurchasePower.setSuccessor(ceoPurchasePower);
+        managerPurchasePower.setSuccessor(directorPurchasePower);
+
+        while (true) {
+            System.out.println("Enter the amount to check who should approve your budget:");
+            System.out.print(">>");
+
+            try {
+                double d = Double.parseDouble(new BufferedReader(new InputStreamReader(System.in)).readLine());
+                managerPurchasePower.processRequest(new PurchaseRequest(d, "By Stuff"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
 
 
-    }
-
-    public Main(InterpreterContext interpreterContext) {
-        this.interpreterContext = interpreterContext;
-    }
-
-    public  String interpret(String str) {
-        Expression expression = null;
-
-        if (str.contains("Hexadecimal")) {
-            expression = new IntToHexExpression(Integer.parseInt(str.substring(0, str.indexOf(" "))));
-        }else if (str.contains("Binary")) {
-            expression = new IntToBinaryExpression(Integer.parseInt(str.substring(0, str.indexOf(" "))));
-        }else return str;
-
-        return expression.interpreter(interpreterContext);
 
     }
 }
