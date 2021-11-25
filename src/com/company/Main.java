2fd68889161ca7bcc46ac27298a2d3e69f57e8d2
Sanika Surve <89775023+sanikasurve@users.noreply.github.com>
2021-11-25 18:23:26 +0530

    feat: flyweight pattern code added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,31 @@
 package com.company;
 
 public class Main {
+    private static final String colors[] = {"Green", "Blue", "Pink", "Yellow"};
+
 
     public static void main(String[] args) {
+          for (int i = 0; i < 20; i++) {
+              Circle circle = (Circle)ShapeFactory.getCircle(getRandomColor());
+              circle.setX(getRandomX());
+              circle.setY(getRandomY());
+              circle.setRadius(400);
+              circle.draw();
 
-        Vehicle bmw = new Car(new Make(), new Assemble());
-        bmw.manufacture();
+          }
 
-        Vehicle bmx = new Bike(new Make(), new Assemble());
-        bmx.manufacture();
+
+
+    }
+
+    private static String getRandomColor() {
+        return colors[(int)(Math.random()*colors.length)];
+    }
+
+    private static int getRandomX() {
+        return (int)(Math.random()*100);
+    }
+    private static int getRandomY() {
+        return (int)(Math.random()*100);
     }
 }
